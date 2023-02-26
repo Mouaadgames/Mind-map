be139a489d57e15294af65dcb37d30954c33beda
Mouaadgames <mouaadgames7@gmail.com>
2023-02-26 09:16:17 +0100

    adding classes

--- JS/sketch.js
+++ JS/sketch.js
@@ -1,3 +1,4 @@
+let nodes = []
 function setup() {
 	//rest option
 	rectMode(CENTER)
@@ -6,27 +7,125 @@ function setup() {
 	setInterval(() => {
 		setCanvasSize()
 	}, 200);
+	//test 
 
 }
 let protoCube = true;
-let elements = []
 function draw() {
 	if (mouseX > width || mouseY > height)
 		return
 	background(100)
-	if (elements.length) {
-		let curentOnWorking = elements[elements.length - 1]
-		curentOnWorking.setPos(mouseX, mouseY)
-	}
-	elements.forEach((cub) => {
+	nodes.forEach((cub) => {
+		cub.checkMouse()
 		cub.show()
 	})
 
 
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//node class
+class Node {
+	constructor() {
+		this.x = 0
+		this.y = 0
+		this.h = 100
+		this.w = 100
+		this.color = color(100, 100, 100)
+		this.isSelected = false
+		this.isHoverd = false
+		this.label = ""
+		this.init = true
+		// [[theNode,bool:inTo or fromTo],[theNode,bool:true]]
+		this.connections = []
+
+	}
+
+
+
+	addConnection(otherNode) {
+		this.connections.push([otherNode, true])
+		otherNode.connections.push([this, false])
+	}
+}
+
+class Cube extends Node {
+	constructor(props) {
+		super(props)
+
+	}
+	show() {
+		if (this.init) {
+			this.x = mouseX
+			this.y = mouseY
+		}
+		let hoverColor = color(100, 100, 255)
+		stroke(this.isHoverd ? hoverColor : 0)
+		strokeWeight(5)
+
+		fill(this.color)
+		rectMode(CENTER)
+		rect(this.x, this.y, this.w, this.h)
+
+		textSize(16)
+		textAlign(CENTER, CENTER)
+		text(this.label, this.x, this.y)
+		for (let connection of this.connections) {
+			line(this.x, this.y, connection.x, connection.y)
+		}
+	}
+
+	checkMouse() {
+		if (!this.init) {
+			this.isHoverd =
+				((this.x + this.w / 2) < mouseX) &&
+				((this.y + this.h / 2) < mouseY) &&
+				((this.x - this.w / 2) > mouseX) &&
+				((this.y - this.h / 2) > mouseY)
+		}
+	}
+
+}
+
 function mouseClicked() {
-	elements.push(new Cube())
+	if (mouseX > width && mouseY > height) {
+		return
+	}
+	for (let node of nodes) {
+		if (node.init) {
+			node.init = false
+			return
+		}
+		if (node.isHoverd) {
+			node.isSelected = true
+			return
+		}
+		// if (dist(node.x, node.y, mouseX, mouseY) < 25) {
+		// 	let label = prompt("Enter node label:");
+		// 	if (label) {
+		// 		let newNode = new Node(mouseX, mouseY, label);
+		// 		nodes.push(newNode);
+		// 		node.addConnection(newNode);
+		// 	}
+		// }
+	}
 }
 
 
@@ -34,40 +133,13 @@ function mouseClicked() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-//elements constructor function
-function Cube() {
-	this.x = 0
-	this.y = 0
-	this.w = 100
-	this.h = 100
-}
-Cube.prototype.setPos = function (x, y) {
-	this.x = x
-	this.y = y
-}
-Cube.prototype.show = function () {
-	rect(this.x, this.y, this.w, this.h)
-}
 
 
 
 // click handler
 
 function addcub() {
-
+	nodes.push(new Cube)
 }
 function addcir() {
 
